4698473b9f890056e911c6861ee54f7b0a4ba84d
Thomas Abraham <thomas.smtp@yahoo.com>
2014-01-05 10:44:06 +0530

    test.java using bufferedInputStream

--- test.java
+++ test.java
@@ -1,3 +1,31 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+
+class TEST {
+	public static void main(String[] args) throws IOException {
+		BufferedInputStream br = new BufferedInputStream(System.in);
+		int input;
+		while ((input = getInt(br)) != 42) {
+			System.out.println(input);
+		}
+		br.close();
+	}
+
+	public static int getInt(BufferedInputStream bis) throws IOException {
+		int result = 0;
+		int input = 0;
+		do {
+			input = bis.read();
+		} while (!Character.isDigit(input));
+		result = input - 48;
+		while (Character.isDigit(input = bis.read())) {
+			result *= 10;
+			result += input - 48;
+		}
+		return result;
+	}
+}
+/*
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,4 +40,4 @@ class TEST {
 		br.close();
 	}
 }
-
+*/
